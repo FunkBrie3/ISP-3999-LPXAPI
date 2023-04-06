358e42a706e3b2c1ecb0321b19c07447b3235a0b
FunkBrie3 <brianhiggins595@gmail.com>
2023-04-06 14:24:32 -0400

    #5
    oh yeah also i merged conway and draw to one project with different compile options

--- src/me/ambrie3/gameoflife/run-conway.kt
+++ src/me/ambrie3/gameoflife/run-conway.kt
@@ -1,5 +1,16 @@
 package me.ambrie3.gameoflife
 
+import me.ambrie3.lpxapi.searchKeyIn
+import me.ambrie3.lpxapi.searchKeyOut
+
 fun main(args: Array<String>) {
-    if(args.size >= 2) TileManager(args[0], args[1]) else TileManager()
+    var inS: String? = null
+    var outS: String? = null
+    for(n in args.indices) {
+        if(args.size - 1 > n) {
+            if(args[n] == "-in" && inS == null) inS = args[n + 1]
+            else if(args[n] == "-out" && outS == null) outS = args[n + 1]
+        }
+    }
+    TileManager(inS ?: searchKeyIn, outS ?: searchKeyOut)
 }
